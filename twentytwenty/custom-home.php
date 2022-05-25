f3f49359908652ce53d2d1183414ec62a4641c3e
ulysses316 <ulises316@live.com.mx>
2022-05-25 12:46:16 -0500

    quieroserasesor + Privacidad

---
+++ twentytwenty/custom-home.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="next-head-count" content="2" />
+    <link rel="stylesheet" href="/wp-content/themes/quieroserasesor/public/styles/provident.css" />
+    <link rel="preload" href="/wp-content/themes/quieroserasesor/public/static/css/50035b4a5c0897bc.css" as="style" />
+    <link rel="stylesheet" href="/wp-content/themes/quieroserasesor/public/static/css/50035b4a5c0897bc.css" data-n-p="" /><noscript data-n-css=""></noscript>
+    <script defer="" nomodule="" src="/wp-content/themes/quieroserasesor/public/static/chunks/polyfills-5cd94c89d3acac5f.js"></script>
+    <script src="/wp-content/themes/quieroserasesor/public/static/chunks/webpack-9b312e20a4e32339.js" defer=""></script>
+    <script src="/wp-content/themes/quieroserasesor/public/static/chunks/framework-5f4595e5518b5600.js" defer=""></script>
+    <script src="/wp-content/themes/quieroserasesor/public/static/chunks/main-a4d9e436b9a532ca.js" defer=""></script>
+    <script src="/wp-content/themes/quieroserasesor/public/static/chunks/pages/_app-7857ddb502a3bd24.js" defer=""></script>
+    <script src="/wp-content/themes/quieroserasesor/public/static/chunks/21-888f1f906a8ad19c.js" defer=""></script>    
+    <script src="/wp-content/themes/quieroserasesor/public/static/mZHGJmG3tZ4LMYdBdtSI2/_buildManifest.js" defer=""></script>
+    <script src="/wp-content/themes/quieroserasesor/public/static/mZHGJmG3tZ4LMYdBdtSI2/_ssgManifest.js" defer=""></script>
+    <script src="/wp-content/themes/quieroserasesor/public/static/mZHGJmG3tZ4LMYdBdtSI2/_middlewareManifest.js" defer=""></script>
+    <style id="__jsx-82b1d2ae7d2628a5">
+        html,
+        body,
+        div,
+        span,
+        applet,
+        object,
+        iframe,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        blockquote,
+        pre,
+        a,
+        abbr,
+        acronym,
+        address,
+        big,
+        cite,
+        code,
+        del,
+        dfn,
+        em,
+        img,
+        ins,
+        kbd,
+        q,
+        s,
+        samp,
+        small,
+        strike,
+        strong,
+        sub,
+        sup,
+        tt,
+        var,
+        b,
+        u,
+        i,
+        center,
+        dl,
+        dt,
+        dd,
+        ol,
+        ul,
+        li,
+        fieldset,
+        form,
+        label,
+        legend,
+        table,
+        caption,
+        tbody,
+        tfoot,
+        thead,
+        tr,
+        th,
+        td,
+        article,
+        aside,
+        canvas,
+        details,
+        embed,
+        figure,
+        figcaption,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        output,
+        ruby,
+        section,
+        summary,
+        time,
+        mark,
+        audio,
+        video {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font-size: 100%;
+            font: inherit;
+            vertical-align: baseline
+        }
+
+        article,
+        aside,
+        details,
+        figcaption,
+        figure,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        section {
+            display: block
+        }
+
+        body {
+            line-height: 1
+        }
+
+        ol,
+        ul {
+            list-style: none
+        }
+
+        blockquote,
+        q {
+            quotes: none
+        }
+
+        blockquote:before,
+        blockquote:after,
+        q:before,
+        q:after {
+            content: "";
+            content: none
+        }
+
+        table {
+            border-collapse: collapse;
+            border-spacing: 0
+        }
+
+        html,
+        body {
+            font-size: 12px;
+            font-family: "Montserrat", sans-serif;
+            text-align: center;
+            background-color: #ecf8f4
+        }
+
+        html.scroll-off,
+        body.scroll-off {
+            height: 100%;
+            overflow: hidden
+        }
+
+        nav {
+            position: fixed;
+            text-align: center;
+            z-index: 99999;
+            background-color: #ecf8f4;
+            left: 1em;
+            top: 1em;
+            width: 64px;
+            height: 64px;
+            border-radius: 32px
+        }
+
+        @media screen and (min-width:720px) {
+            nav {
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: auto;
+                border-radius: 0;
+                background: linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, #dff3ed 25%, #dff3ed 100%)
+            }
+        }
+
+        nav .mobile-menu {
+            width: 32px;
+            height: 32px;
+            display: -webkit-inline-box;
+            display: -webkit-inline-flex;
+            display: -ms-inline-flexbox;
+            display: inline-flex;
+            -webkit-flex-direction: column;
+            -ms-flex-direction: column;
+            flex-direction: column;
+            -webkit-align-items: center;
+            -webkit-box-align: center;
+            -ms-flex-align: center;
+            align-items: center;
+            -webkit-justify-content: space-around;
+            justify-content: space-around;
+            margin-top: 14px;
+            position: relative
+        }
+
+        nav .mobile-menu.active {
+            z-index: 999999
+        }
+
+        nav .mobile-menu.active span {
+            background-color: white
+        }
+
+        nav .mobile-menu.active span:first-child {
+            -webkit-transform: rotate(45deg) translateY(4px) translateX(3px);
+            -moz-transform: rotate(45deg) translateY(4px) translateX(3px);
+            -ms-transform: rotate(45deg) translateY(4px) translateX(3px);
+            transform: rotate(45deg) translateY(4px) translateX(3px)
+        }
+
+        nav .mobile-menu.active span:nth-child(2) {
+            display: none
+        }
+
+        nav .mobile-menu.active span:last-child {
+            -webkit-transform: rotate(-45deg) translateY(-8px) translateX(8px);
+            -moz-transform: rotate(-45deg) translateY(-8px) translateX(8px);
+            -ms-transform: rotate(-45deg) translateY(-8px) translateX(8px);
+            transform: rotate(-45deg) translateY(-8px) translateX(8px)
+        }
+
+        nav .mobile-menu span {
+            width: 100%;
+            height: 3px;
+            border-radius: 1.5px;
+            background-color: #106DC0
+        }
+
+        @media screen and (min-width:720px) {
+            nav .mobile-menu {
+                display: none
+            }
+        }
+
+        nav ul {
+            display: none
+        }
+
+        @media screen and (min-width:720px) {
+            nav ul {
+                display: -webkit-box;
+                display: -webkit-flex;
+                display: -ms-flexbox;
+                display: flex;
+                margin: auto;
+                margin-top: 4em;
+                margin-bottom: 2em;
+                width: 100%;
+                max-width: 1140px;
+                -webkit-flex-direction: row;
+                -ms-flex-direction: row;
+                flex-direction: row
+            }
+        }
+
+        nav ul.shown {
+            position: fixed;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            color: white;
+            display: -webkit-box;
+            display: -webkit-flex;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-flex-direction: column;
+            -ms-flex-direction: column;
+            flex-direction: column;
+            text-align: left;
+            padding-top: 6em
+        }
+
+        nav ul.shown li {
+            font-size: 1.4em;
+            padding: 0em 2em;
+            position: relative;
+            z-index: 9999
+        }
+
+        nav ul.shown li a {
+            padding: 1em 0em;
+            display: inline-block;
+            width: 100%;
+            border-top: 1px #FFF4 solid;
+            color: white
+        }
+
+        nav ul.shown li:first-child a {
+            border-top: none
+        }
+
+        nav ul.shown::before {
+            content: "";
+            position: fixed;
+            background-color: #444B;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            top: 0;
+            z-index: 9
+        }
+
+        nav ul.shown::after {
+            content: "";
+            position: absolute;
+            background-color: #77bb14;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            top: 0;
+            z-index: 99
+        }
+
+        nav ul li {
+            font-size: 1.5em;
+            text-transform: uppercase;
+            font-weight: 500;
+            padding: 0 1em
+        }
+
+        nav ul li a {
+            color: #106DC0;
+            -webkit-text-decoration: none;
+            text-decoration: none
+        }
+
+        nav ul li a.selected {
+            color: #bbd04e
+        }
+
+        nav ul li a:hover {
+            color: #BAE259
+        }
+    </style>
+    <style id="__jsx-a1b951e7fcca964f">
+        footer {
+            position: initial;
+            height: 100%;            
+        }
+    </style>
+</head>
+
+<body lang="es">
+    <div id="__next" data-reactroot="">
+        <div id="asesores_root__2fYaP" class="jsx-82b1d2ae7d2628a5">
+            <nav class="jsx-82b1d2ae7d2628a5 nav">
+                <div id="mobile-menu" class="jsx-82b1d2ae7d2628a5 mobile-menu"><span class="jsx-82b1d2ae7d2628a5"></span><span class="jsx-82b1d2ae7d2628a5"></span><span class="jsx-82b1d2ae7d2628a5"></span></div>
+                <ul id="nav-menu" class="jsx-82b1d2ae7d2628a5">
+                    <li class="jsx-82b1d2ae7d2628a5"><a href="#asesores_intro__KEpx2" class="jsx-82b1d2ae7d2628a5 ">Intro</a></li>
+                    <li class="jsx-82b1d2ae7d2628a5"><a href="#asesores_que-es__s10y5" class="jsx-82b1d2ae7d2628a5 ">¿Qué es?</a></li>
+                    <li class="jsx-82b1d2ae7d2628a5"><a href="#asesores_expectativas___uddE" class="jsx-82b1d2ae7d2628a5 ">¿Qué buscamos?</a></li>
+                    <li class="jsx-82b1d2ae7d2628a5"><a href="#actividades-a-realizar" class="jsx-82b1d2ae7d2628a5 ">Las
+                            actividades a realizar</a></li>
+                    <li class="jsx-82b1d2ae7d2628a5"><a href="#asesores_beneficios__jx_aR" class="jsx-82b1d2ae7d2628a5 ">Beneficios</a></li>
+                    <li class="jsx-82b1d2ae7d2628a5"><a href="#asesores_que-sigue__9c_tt" class="jsx-82b1d2ae7d2628a5 ">¿Qué sigue?</a></li>
+                </ul>
+            </nav>
+
+            <?php
+                $page_id = get_option('page_on_front');
+                $page = get_post($page_id);
+                $content = $page->post_content;
+                echo $content;
+            ?>
+
+            <footer class="jsx-a1b951e7fcca964f">
+                <div class="jsx-a1b951e7fcca964f copyright-container">
+                    <div class="jsx-a1b951e7fcca964f container">
+                        <div class="jsx-a1b951e7fcca964f row">
+                            <div class="jsx-a1b951e7fcca964f col-xs-12">
+                                <ul class="jsx-a1b951e7fcca964f nav navbar-nav">
+                                    <li class="jsx-a1b951e7fcca964f"><a href="/privacidad" class="jsx-a1b951e7fcca964f">Aviso de Privacidad</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <script id="__NEXT_DATA__" type="application/json">
+            {
+                "props": {
+                    "pageProps": {}
+                },
+                "page": "/landings/asesor",
+                "query": {},
+                "buildId": "mZHGJmG3tZ4LMYdBdtSI2",
+                "nextExport": true,
+                "autoExport": true,
+                "isFallback": false,
+                "scriptLoader": []
+            }
+        </script>
+</body>
+
+</html>
